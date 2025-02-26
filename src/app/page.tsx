25013beb8d2e9d9697126d0206d9d67ccdac2d26
Matthias Seidl <matthias@mseidl-analytics.de>
2025-02-26 15:53:18 +0100

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ const HomePage: FC = () => {
             <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl md:text-6xl">
             Open Labels Initiative
             </h1>
-            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+            <p className="mt-4 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:text-xl md:max-w-3xl">
             A standardized framework and data model for EVM address labeling
             </p>
-            <div className="mt-5 mx-auto sm:flex sm:justify-center md:mt-8">
+            <div className="mt-5 mx-auto sm:flex sm:justify-center">
             {/* GitHub Repository Button */}
             <div className="rounded-md shadow">
                 <a

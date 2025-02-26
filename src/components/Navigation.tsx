3255aee9518517479c4d11fb05aeab9459bccca5
Matthias Seidl <matthias@mseidl-analytics.de>
2025-02-26 20:52:53 +0100

    moved OLI logo back to footer

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -107,15 +107,6 @@ const Navigation: React.FC = () => {
     <nav className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          {/* Logo/Home link */}
-          <div className="flex items-center">
-            <img 
-              src="/oli-logo.png" 
-              alt="Open Labels Initiative" 
-              className="h-10 w-auto object-contain cursor-pointer hover:opacity-90 transition-opacity duration-200"
-              onClick={() => window.location.href = '/'}
-            />
-        </div>
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex">

490edc3522970f6845bc8c29134b56c0cc3c2c5a
Matthias Seidl <matthias@mseidl-analytics.de>
2025-03-26 16:00:59 +0100

    small text

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -9,7 +9,7 @@ export default function AddProjectPage() {
         <div className="py-6 px-8">
           <h2 className="text-2xl font-bold text-gray-900 mb-4">Add a New Project</h2>
           <p className="text-gray-700 mb-4">
-            Add a new project to the OSS (Open Source Software) directory which is used by OLI. The project information will be generated as a YAML file that can be downloaded.
+            Add a new project to the OSS (Open Source Software) directory which is used by OLI. The project from will generate a YAML file that can be added to the OSS repo via a GitHub pull request.
           </p>
           <Link 
             href="https://github.com/opensource-observer/oss-directory"
@@ -24,7 +24,7 @@ export default function AddProjectPage() {
       
       <div className="max-w-4xl mx-auto bg-white rounded-2xl shadow-[0_8px_30px_rgb(0,0,0,0.05)]">
         <div className="flex justify-between items-center pt-6 pb-4 px-8">
-          <h2 className="text-2xl font-bold text-gray-900">Project Information Form</h2>
+          <h2 className="text-2xl font-bold text-gray-900">Project Form</h2>
         </div>
         <AddProjectForm />
       </div>
